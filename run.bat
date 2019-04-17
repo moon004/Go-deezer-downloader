763dce9070da353638793568b8a021116a2e1ebc
moon004 <herodotus94@gmail.com>
2019-04-17 15:59:17 +0800

    Update windows script for docker

---
+++ run.bat
@@ -0,0 +1,14 @@
+ECHO OFF
+ECHO Building app
+
+docker run --rm -v %CD%/src:/src -w /src golang:1.8 go build -v -o app
+
+ECHO Creating image
+
+docker build -t dfs-scratch -f Dockerfile.run .
+
+ECHO Running container
+
+docker run --rm dfs-scratch
+
+docker rmi dfs-scratch
