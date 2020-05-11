9c9ad64bcd2a041079a427b3639673381b933c41
moon004 <herodotus94@gmail.com>
2020-05-11 13:38:22 +0900

    Remove usertoken, only use hardcode usertoken

--- config.go
+++ config.go
@@ -29,15 +29,13 @@ func debug(msg string, params ...interface{}) {
 // ErrorUsage lets the user knows the error
 func ErrorUsage() {
 	fmt.Println(`Guide: go-decrypt-deezer [--debug --id --usertoken`)
-	fmt.Println(`How Do I Get My UserToken?: https://notabug.org/RemixDevs/DeezloaderRemix/wiki/Login+via+userToken`)
-	fmt.Println(`Example: go-decrypt-deezer --id 3135556 --usertoken UserToken_here`)
+	fmt.Println(`Example: go-decrypt-deezer --id 3135556`)
 	flag.PrintDefaults()
 	os.Exit(1)
 }
 
 func init() {
 	flag.BoolVar(&cfg.Debug, "debug", false, "Turn on debuging mode.")
-	flag.StringVar(&cfg.UserToken, "usertoken", "", "Your Unique User Token")
 	flag.StringVar(&cfg.ID, "id", "", "Deezer Track ID")
 
 	flag.Parse()
@@ -46,15 +44,10 @@ func init() {
 	debug("Configuration:")
 
 	debug("\tDebug: %t", cfg.Debug)
-	debug("\tUserToken: %s", cfg.UserToken)
 	debug("\tID: %s", cfg.ID)
 
 	if cfg.ID == "" {
 		fmt.Println("Error: Must have Deezer Track(Song) ID")
-		ErrorUsage()
-	}
-	if cfg.UserToken == "" {
-		fmt.Println("Error: Must have UserToken")
 		ErrorUsage()
 	}
 
